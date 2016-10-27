e1982bc6ae7c02ebdcaa6e080fe2182d177bfb27
wilmergil <wilmergil@live.com>
2016-10-27 09:01:53 -0500

    Animacion
    Se realiza una animacion

---
+++ Assets/Scripts/Funcion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Funcion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void mostrarMensaje()
+	{
+		Debug.Log ("Entra a la funcion");
+	}
+}
